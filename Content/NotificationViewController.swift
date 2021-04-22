ee6c7d5542871aa1ac068420e28481f3b94ca563
Freerider-song <ghthd97@naver.com>
2021-04-22 13:10:24 +0900

    버전 1.2.0

---
+++ Content/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  Content
+//
+//  Created by (주)에너넷 on 2021/01/27.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
